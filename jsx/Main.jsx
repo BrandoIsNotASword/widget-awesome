829e90c8f9daa94671eb669f568bdbfb325a5a13
Brando Pérez Pacheco <brando@ubuntu.(none)>
2015-10-24 15:11:14 -0700

    Component finished

---
+++ jsx/Main.jsx
@@ -0,0 +1,10 @@
+var React = require('react')
+var Like = require('./Like.jsx')
+
+module.exports = React.createClass({
+  'displayName': 'BlankPage.jsx',
+
+  render: function () {
+    return <Like />
+  }
+})
